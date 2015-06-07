626714ab59aa01d89d8a125499e10aef5273d977
Tom Zellman <tzellman@gmail.com>
2015-06-07 05:56:03 -0400

    protect against empty string

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,6 +1,9 @@
 module.exports = function (input) {
-    return (input || '').split(/\s+/).reduce(function (map, p) {
-        map[p] = map.hasOwnProperty(p) ? map[p] + 1 : 1;
+    return (input || '').trim().split(/\s+/).reduce(function (map, p) {
+        // an empty input string will still split to an array of size 1 empty string..
+        if (p) {
+            map[p] = map.hasOwnProperty(p) ? map[p] + 1 : 1;
+        }
         return map;
     }, {});
 };
