4e46ac10c117d8e489bd98d2cc2c7c579d707a46
Tom Zellman <tzellman@gmail.com>
2023-12-21 10:02:58 -0500

    chore: high-score

--- elixir/high-score/lib/high_score.ex
+++ elixir/high-score/lib/high_score.ex
@@ -1,7 +1,9 @@
 defmodule HighScore do
+  @initial_score 0
+
   def new(), do: %{}
 
-  def add_player(scores, name, score \\ 0) do
+  def add_player(scores, name, score \\ @initial_score) do
     scores |> Map.put(name, score)
   end
 
@@ -10,10 +12,10 @@ defmodule HighScore do
   end
 
   def reset_score(scores, name) do
-    scores |> Map.update(name, 0, fn _ -> 0 end)
+    scores |> Map.update(name, @initial_score, fn _ -> @initial_score end)
   end
 
-  def update_score(scores, name, score \\ 0) do
+  def update_score(scores, name, score) do
     scores |> Map.update(name, score, &(&1 + score))
   end
 
