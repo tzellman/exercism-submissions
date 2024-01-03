4487497668f41b437bbe3b7341cf1dac4045ffd7
Tom Zellman <tzellman@gmail.com>
2024-01-03 07:09:44 -0500

    chore: etl

---
+++ elixir/etl/lib/etl.ex
@@ -0,0 +1,18 @@
+defmodule ETL do
+  @doc """
+  Transforms an old Scrabble score system to a new one.
+
+  ## Examples
+
+    iex> ETL.transform(%{1 => ["A", "E"], 2 => ["D", "G"]})
+    %{"a" => 1, "d" => 2, "e" => 1, "g" => 2}
+  """
+  @spec transform(map) :: map
+  def transform(input) do
+    Enum.reduce(input, %{}, fn {score, letters}, acc ->
+      Enum.reduce(letters, acc, fn letter, acc ->
+        Map.put(acc, String.downcase(letter), score)
+      end)
+    end)
+  end
+end
